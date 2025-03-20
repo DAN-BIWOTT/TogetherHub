bf715278351701da0dc2266143471ba6b34eabfb
jashanddhillon <jashan.d.dhillon@gmail.com>
2025-03-20 03:41:59 +0000

    added quetions to forgot password page

--- AuthenticationApp/forms.py
+++ AuthenticationApp/forms.py
@@ -3,9 +3,24 @@ from .models import CustomUser  # Import your custom user model
 
 class CustomPasswordResetForm(forms.Form):
     email = forms.EmailField(label="Email", required=True)
-    security_answer = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your answer'}), max_length=255)
+    security_question = forms.ChoiceField(
+        label="Security Question",
+        choices=[
+            ("", "Select security question"),
+            ("What is your favorite food?", "What is your favorite food?"),
+            ("What is your favorite place?", "What is your favorite place?"),
+            ("What is your favorite hero?", "What is your favorite hero?"),
+            ("What is your favorite colour?", "What is your favorite colour?")
+        ],
+        required=True
+    )
+    # security_answer = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your answer'}), max_length=255)
 
-
+    security_answer = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Your answer', 'autocomplete': 'off'}),
+        max_length=255,
+        required=True
+    )
     def clean(self):
         cleaned_data = super().clean()
         email = cleaned_data.get("email")
